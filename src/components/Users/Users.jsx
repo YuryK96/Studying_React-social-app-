0b4acdd97a3b97a62d01f2c29537528712b3250a
YuryK96 <kast2406@gmail.com>
2022-11-16 13:49:17 +0300

    changed functional component to class component Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,70 +1,67 @@
-import axios from "axios";
+import React from "react";
 import s from "./Users.module.scss";
+import axios from "axios";
+class Users extends React.Component {
+  componentDidMount() {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
 
-const Users = ({ users, onFollow, onUnFollow, setUsers }) => {
-  let getUsers = () => {
-    if (users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          setUsers(response.data.items);
-        });
+    // 72e23854-f5e3-4835-a7c4-abe54c7e9e9c
+  }
 
-      // 72e23854-f5e3-4835-a7c4-abe54c7e9e9c
-    }
-  };
-
-  return (
-    <div>
-      <button onClick={getUsers}>add Users</button>
-      {users.map((u) => {
-        console.log(u.photos.small);
-        return (
-          <section key={u.id}>
-            <div className={s.user}>
-              <div className={s.user__photo}>
-                <img
-                  src={
-                    u.photos.small != null
-                      ? u.photos.small
-                      : "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
-                  }
-                  alt=""
-                />
-                {u.followed ? (
-                  <button
-                    onClick={() => {
-                      onFollow(u.id);
-                    }}
-                  >
-                    UnFollow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      onUnFollow(u.id);
-                    }}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
-              <div className={s.user__container}>
-                <div className={s.user__containerInfo}>
-                  <div>{u.fullName}</div>
-                  <div>{u.status}</div>
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => {
+          return (
+            <section key={u.id}>
+              <div className={s.user}>
+                <div className={s.user__photo}>
+                  <img
+                    src={
+                      u.photos.small != null
+                        ? u.photos.small
+                        : "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
+                    }
+                    alt=""
+                  />
+                  {u.followed ? (
+                    <button
+                      onClick={() => {
+                        this.props.onFollow(u.id);
+                      }}
+                    >
+                      UnFollow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        this.props.onUnFollow(u.id);
+                      }}
+                    >
+                      Follow
+                    </button>
+                  )}
                 </div>
-                <div className={s.user__containerLocation}>
-                  <div>{"u.location.city"}</div>
-                  <div>{"u.location.country"}</div>
+                <div className={s.user__container}>
+                  <div className={s.user__containerInfo}>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
+                  </div>
+                  <div className={s.user__containerLocation}>
+                    <div>{"u.location.city"}</div>
+                    <div>{"u.location.country"}</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </section>
-        );
-      })}
-    </div>
-  );
-};
-
+            </section>
+          );
+        })}
+      </div>
+    );
+  }
+}
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -36,7 +36,7 @@ const usersReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state,
-        users: [...state.users, ...action.users],
+        users: [...action.users],
       };
 
     default:
