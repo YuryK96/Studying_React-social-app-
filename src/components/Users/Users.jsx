0ea9e78ccc43fca94777f732dbfa65b6ef2998ee
YuryK96 <kast2406@gmail.com>
2022-11-17 21:09:02 +0300

    created presentational component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,106 +1,88 @@
-import React from "react";
 import s from "./Users.module.scss";
-import axios from "axios";
-class Users extends React.Component {
-  componentDidMount() {
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUserCount(response.data.totalCount);
-      });
 
-    // 72e23854-f5e3-4835-a7c4-abe54c7e9e9c
+const Users = ({
+  onPageChanged,
+  currentPage,
+  users,
+  onUnFollow,
+  onFollow,
+  totalUsersCount,
+  pageSize,
+}) => {
+  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+  let pages = [];
+
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  onPageChanged = (page) => {
-    this.props.onCurrentPage(page);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-      });
-  };
-  render() {
-    let pagesCount = Math.ceil(
-      this.props.totalUsersCount / this.props.pageSize
-    );
-    let pages = [];
-
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    return (
-      <div>
+  return (
+    <div>
+      <div className={s.pagination}>
         {pages.map((p, i) => {
           return (
             <span
               key={i}
               id={i}
               onClick={() => {
-                this.onPageChanged(p);
+                onPageChanged(p);
               }}
-              className={` ${s.page}  ${
-                this.props.currentPage === p ? s.selectedPage : null
+              className={` ${s.pagination__page}  ${
+                currentPage === p ? s.selectedPage : null
               }`}
             >
               {p}
             </span>
           );
         })}
-
-        {this.props.users.map((u) => {
-          return (
-            <section key={u.id}>
-              <div className={s.user}>
-                <div className={s.user__photo}>
-                  <img
-                    src={
-                      u.photos.small != null
-                        ? u.photos.small
-                        : "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
-                    }
-                    alt=""
-                  />
-                  {u.followed ? (
-                    <button
-                      onClick={() => {
-                        this.props.onFollow(u.id);
-                      }}
-                    >
-                      UnFollow
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() => {
-                        this.props.onUnFollow(u.id);
-                      }}
-                    >
-                      Follow
-                    </button>
-                  )}
+      </div>
+      {users.map((u) => {
+        return (
+          <section key={u.id}>
+            <div className={s.user}>
+              <div className={s.user__photo}>
+                <img
+                  src={
+                    u.photos.small != null
+                      ? u.photos.small
+                      : "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
+                  }
+                  alt=""
+                />
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      onFollow(u.id);
+                    }}
+                  >
+                    UnFollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      onUnFollow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
+              <div className={s.user__container}>
+                <div className={s.user__containerInfo}>
+                  <div>{u.name}</div>
+                  <div>{u.status}</div>
                 </div>
-                <div className={s.user__container}>
-                  <div className={s.user__containerInfo}>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                  </div>
-                  <div className={s.user__containerLocation}>
-                    <div>{"u.location.city"}</div>
-                    <div>{"u.location.country"}</div>
-                  </div>
+                <div className={s.user__containerLocation}>
+                  <div>{"u.location.city"}</div>
+                  <div>{"u.location.country"}</div>
                 </div>
               </div>
-            </section>
-          );
-        })}
-      </div>
-    );
-  }
-}
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
+};
+
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -6,7 +6,48 @@ import {
   setUsersAC,
   unfollowAC,
 } from "../../redux/users-reducer";
+import axios from "axios";
 import Users from "./Users";
+import React from "react";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUserCount(response.data.totalCount);
+      });
+
+    // 72e23854-f5e3-4835-a7c4-abe54c7e9e9c
+  }
+
+  onPageChanged = (page) => {
+    this.props.onCurrentPage(page);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        onPageChanged={this.onPageChanged}
+        currentPage={this.props.currentPage}
+        users={this.props.users}
+        onFollow={this.props.onFollow}
+        onUnFollow={this.props.onUnFollow}
+      />
+    );
+  }
+}
 
 let mapStateToProps = (state) => {
   return {
@@ -32,6 +73,4 @@ let mapDispatchToProps = (dispatch) => {
     },
   };
 };
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
