f8bb3cdcf8b4bf820efd12ac9d2eefa9cd3f3823
YuryK96 <kast2406@gmail.com>
2022-11-25 19:37:37 +0300

    added ComponentDidUpdater in ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -18,7 +18,11 @@ class ProfileStatus extends React.Component {
   onStatusChange = (e) => {
     this.setState({ status: e.currentTarget.value });
   };
-
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({ status: this.props.status });
+    }
+  }
   render() {
     return (
       <div>
